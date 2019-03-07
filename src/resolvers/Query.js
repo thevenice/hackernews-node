1d7896a1a3f90571d0a0433f1bd0879cf06fb52e
thevenice <pp886655@gmail.com>
2019-03-07 20:05:00 +0530

    Query/linkCount

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -18,6 +18,19 @@ async function feed(parent, args, context, info) {
     last: args.last,
     orderBy: args.orderBy
   });
-  return links;
+  const count = await context.prisma
+    .linksConnection({
+      where
+    })
+    .aggregate()
+    .count();
+  return { links, count };
 }
 module.exports = { feed };
+
+/**
+ * links and count
+ * You’re first using the provided filtering, ordering and pagination arguments to retrieve a number of Link elements.
+Next, you’re using the linksConnection query from the Prisma client API to retrieve the total number of Link elements currently stored in the database.
+The links and count are then wrapped in an object to adhere to the Feed type that you just added to the GraphQL schema.
+ */
